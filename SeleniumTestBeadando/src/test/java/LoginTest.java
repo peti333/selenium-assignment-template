5568e0a28423f04abce7d830f88920d21b002fae
peti333 <makkpetiek@gmail.com>
2026-05-15 11:53:03 +0200

    Attempt at setting up tests and selenium

---
+++ SeleniumTestBeadando/src/test/java/LoginTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
+import java.net.MalformedURLException;
+
+public class LoginTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private WebElement waitVisibilityAndFindElement(By locator) {
+        this.wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return this.driver.findElement(locator);
+    }
+
+    @Before
+    public void setup() throws MalformedURLException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), options);
+        driver.manage().window().maximize();
+
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void testLoginSuccessAndLogout() {
+        this.driver.get("https://letterboxd.com/");
+
+        By SignInLocator = By.className("navitem sign-in-menu");
+
+        WebElement SignInElement = waitVisibilityAndFindElement(SignInLocator);
+        Assert.assertTrue(SignInElement.getText().contains("Sign in"));
+
+    }
+
+}
